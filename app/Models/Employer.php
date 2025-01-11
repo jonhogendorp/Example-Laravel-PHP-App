dc8e33b4fcf4d93775b972d01fa7751425f8310d
Jon Hogendorp <64735813+jonhogendorp@users.noreply.github.com>
2025-01-11 17:38:40 +0100

    relationship types

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -9,4 +9,10 @@ class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function jobs(){
+        return $this->hasMany(Job::class);
+    }
 }
