ff42c9bbb020d2411b4172b9f5b913e5bdc423e7
Jon Hogendorp <64735813+jonhogendorp@users.noreply.github.com>
2025-01-15 23:48:49 +0100

    card

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,11 +2,13 @@
     <x-slot:heading>About Page</x-slot>
 
     <h1>hello from the about page</h1>
-    <ul>
-        @foreach ($jobs as $job  )
-            <a href="/jobs/{{$job['id']}}">
-                <li class="my-5 text-blue-500 hover:underline underline-offset-2">
-                    <strong>{{$job['title']}}:</strong> Pays {{$job['salary']}} per year
+    <ul class="mt-5 space-y-4">
+        @foreach ($jobs as $job)
+            <a href="/jobs/{{ $job['id'] }}">
+                <li class="border- my-5 block rounded-lg border border-gray-200 px-4 py-6">
+                    <div class="">{{ $job->employer->name }}</div>
+                    <strong>{{ $job['title'] }}:</strong>
+                    Pays {{ $job['salary'] }} per year
                 </li>
             </a>
         @endforeach
