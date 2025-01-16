b24e47488f46b89a9560ead909bee4ec975c7c73
Jon Hogendorp <64735813+jonhogendorp@users.noreply.github.com>
2025-01-16 22:56:42 +0100

    eager loading plus debug bar install

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,8 +5,8 @@
     <ul class="mt-5 space-y-4">
         @foreach ($jobs as $job)
             <a href="/jobs/{{ $job['id'] }}">
-                <li class="border- my-5 block rounded-lg border border-gray-200 px-4 py-6">
-                    <div class="">{{ $job->employer->name }}</div>
+                <li class="border- my-5 block rounded-lg border border-gray-200 bg-slate-50 px-4 py-6 shadow">
+                    <div class="text-blue-500">{{ $job->employer->name }}</div>
                     <strong>{{ $job['title'] }}:</strong>
                     Pays {{ $job['salary'] }} per year
                 </li>
