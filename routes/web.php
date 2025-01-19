544539967963dfb35ca795f94f514c7e25c9e9a9
Jon Hogendorp <64735813+jonhogendorp@users.noreply.github.com>
2025-01-19 23:05:10 +0100

    csrf laravel part 1

--- routes/web.php
+++ routes/web.php
@@ -12,20 +12,25 @@ Route::get('/', function () {
 Route::get('/jobs', function ()  {
     $jobs = Job::with('employer')->simplePaginate(3);
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id)  {
+Route::get('jobs/create', function(){
+    return view('jobs.create');
+});
+
+Route::get('/jobs.{id}', function ($id)  {
 
     $job = Job::find($id);
 
-    return view('job', [
+    return view('job.show', [
         'job' => $job
     ]);
 
 });
+
 
 Route::get('/contact', function () {
     return view('contact');
