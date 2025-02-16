1b58c91896ba4509f47505b5897f2abfba9cc8b9
Jon Hogendorp <64735813+jonhogendorp@users.noreply.github.com>
2025-02-16 16:15:24 +0100

    Refactor job routes to use resource controller; implement JobController for CRUD operations and update view handling

--- routes/web.php
+++ routes/web.php
@@ -1,97 +1,10 @@
 <?php
 
-
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
-
-Route::get('/', function () {
-
-    return view('home');
-});
 
 
-// Index page
-Route::get('/jobs', function ()  {
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-//Create page jobs
-Route::get('jobs/create', function(){
-    return view('jobs.create');
-});
-
-
-// Show page jobs
-Route::get('/jobs/{id}', function ($id)  {
-
-    $job = Job::find($id);
-
-    return view('jobs.show', [
-        'job' => $job
-    ]);
-
-});
-
-// store jobs
-Route::post('/jobs', function(){
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-// Edit page jobs
-Route::get('/jobs/{id}/edit', function ($id)  {
-
-    $job = Job::find($id);
-
-    return view('jobs.edit', [
-        'job' => $job
-    ]);
-
-});
-
-// Update jobs
-Route::patch('/jobs/{id}', function ($id)  {
-//validate
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary')
-    ]);
-
-    return redirect('/jobs/' . $job->id);
-
-});
-
-// Delete jobs
-Route::delete('/jobs/{id}', function ($id)  {
-
-    $job = Job::findOrFail($id);
-
-    $job->delete();
-
-    return redirect('/jobs');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::resource('jobs', JobController::class);
