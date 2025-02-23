e1186aaee1c0b410e0379d3459561def3abaf046
Jon Hogendorp <64735813+jonhogendorp@users.noreply.github.com>
2025-02-23 01:31:58 +0100

    Refactor job creation form to use Blade components for improved readability and error handling

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -11,42 +11,25 @@
                 </p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
-                        <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white px-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
-                            >
-
-                                <input
-                                    type="text"
-                                    name="title"
-                                    id="title"
-                                    class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
-                                    placeholder="Shift leader"
-                                    {{-- required --}}
-                                />
+                    <x-form-field>
+                        <x-form-label for="title" >Title</x-form-label>
+                            <div class="mt-2">
+                                <x-form-input name="title" id="title" placeholder="Shift leader" />
+                                <x-form-error name="title" />
                             </div>
-                        </div>
-                    </div>
-
-                    <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
+                    </x-form-field>
+                    <x-form-field>
+                        <x-form-label for="salary">Salary</x-form-label>
                         <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white px-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
-                            >
-
-                                <input
-                                    type="text"
-                                    name="salary"
-                                    id="salary"
-                                    class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
-                                    placeholder="$50.000"
-                                />
-                            </div>
+                            <x-form-input
+                                name="salary"
+                                id="salary"
+                                class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+                                placeholder="$50.000"
+                            />
+                            <x-form-error name="salary" />
                         </div>
-                    </div>
+                    </x-form-field>
                     @if($errors->any())
                         <ul>
                             @foreach($errors->all() as $error)
